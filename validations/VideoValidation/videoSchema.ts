a3304cbf46f00418420b2c1b49b6a420b0d4eb81
Cold <kholodihor@gmail.com>
2023-05-29 11:06:02 +0300

    some fixes

--- validations/VideoValidation/videoSchema.ts
+++ validations/VideoValidation/videoSchema.ts
@@ -3,6 +3,6 @@ import Joi from 'joi';
 export const videoSchema = {
   addVideo: Joi.object({
     title: Joi.string().required(),
-    url: Joi.string().required()
+    url: Joi.string().uri().required()
   }),
 };
